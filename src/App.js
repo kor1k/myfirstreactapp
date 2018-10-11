697c4848496472e8e6a2e1e0f92a88f87edc16c2
Kostya <koortttt@gmail.com>
2018-10-11 21:00:28 +0300

    react is very cool. i like it so much

--- src/App.js
+++ src/App.js
@@ -7,27 +7,44 @@ import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
 
 
-
 class App extends Component {
+    state = {
+        userName: "Konstantyn",
+        userAge: 19,
+        // userPlace: 'Kyiv'
+    };
+
+    nameChanged (newValue) {
+        debugger;
+        this.setState({userName: newValue})
+    }
+
     render() {
+        // const {userName, userAge} = this.state;
+        console.log('App', this);
+
         return (
             <div className="App">
-                <Header/>
-                <Body/>
+                <Header name={this.state.userName} age={this.state.userAge}/>
+                <button onClick={() => this.setState({userAge: this.state.userAge + 1})}>Increment age</button>
+
+
+                {/*()=>   ---- стрелочная ф-я ЕС 2015 (learnjavascript.ru)*/}
+                <Body nameChanged = {this.nameChanged.bind(this)} />
                 <Footer/>
                 {/*<header className="App-header">*/}
-                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-                    {/*<p>*/}
-                        {/*Edit <code>src/App.js</code> and save to reload.*/}
-                    {/*</p>*/}
-                    {/*<a*/}
-                        {/*className="App-link"*/}
-                        {/*href="https://reactjs.org"*/}
-                        {/*target="_blank"*/}
-                        {/*rel="noopener noreferrer"*/}
-                    {/*>*/}
-                        {/*Learn React*/}
-                    {/*</a>*/}
+                {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                {/*<p>*/}
+                {/*Edit <code>src/App.js</code> and save to reload.*/}
+                {/*</p>*/}
+                {/*<a*/}
+                {/*className="App-link"*/}
+                {/*href="https://reactjs.org"*/}
+                {/*target="_blank"*/}
+                {/*rel="noopener noreferrer"*/}
+                {/*>*/}
+                {/*Learn React*/}
+                {/*</a>*/}
                 {/*</header>*/}
             </div>
         );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
 
 class Header extends Component {
-    render () {
-        return <h1>Header блок</h1>
+    render() {
+        const {name, age} = this.props;
+        console.log('Header', this);
+        return (
+            <div>
+                <h1>Privet, {name}</h1>
+                <h2>Your age is: {age} </h2>
+
+                {/*<h2>You are in: {city} </h2>*/}
+            </div>
+        )
     }
 }
 
